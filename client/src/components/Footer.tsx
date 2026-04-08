008f8ce4af59de73497563d339dc1dbcf0d75e13
William <billywei0925@gmail.com>
2026-04-08 04:33:57 -0400

    changed email to <a>

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,41 +1,8 @@
-import { useEffect, useState } from "react";
-
 export function Footer({ className = "" }: { className?: string }) {
-  const [email, setEmail] = useState("");
-  useEffect(() => {
-    const parts = [
-      String.fromCharCode(119), 
-      String.fromCharCode(105), 
-      String.fromCharCode(108), 
-      String.fromCharCode(108), 
-      String.fromCharCode(105), 
-      String.fromCharCode(97), 
-      String.fromCharCode(109),
-      String.fromCharCode(119), 
-      String.fromCharCode(101), 
-      String.fromCharCode(105), 
-      String.fromCharCode(57), 
-      String.fromCharCode(56), 
-      String.fromCharCode(53), 
-      String.fromCharCode(55), 
-      String.fromCharCode(64), 
-      String.fromCharCode(103), 
-      String.fromCharCode(109), 
-      String.fromCharCode(97), 
-      String.fromCharCode(105), 
-      String.fromCharCode(108),
-      String.fromCharCode(46), 
-      String.fromCharCode(99), 
-      String.fromCharCode(111),
-      String.fromCharCode(109), 
-    ];
-    const assembled = parts.reduce((acc, part) => acc + part, "");
-    setEmail(assembled);
-  }, []);
-
   return (
     <footer className={`bg-gray-200 border-t border-black py-8 px-8 w-full ${className}`}>
       <div className="mx-20 flex justify-between items-start gap-12">
+        {/* Left: branding + description */}
         <div className="max-w-95">
           <span className="text-2xl font-black tracking-tighter text-blue-900 block mb-6">
             Physical Chemistry - <div>CM-UY 3113</div>
@@ -50,13 +17,7 @@ export function Footer({ className = "" }: { className?: string }) {
             <h5 className="font-bold uppercase text-primary mb-4">Contact</h5>
             <div className="mb-2">For questions and inquires or to report issues, please contact:</div>
             <div> William Wei</div>
-            <div>
-              {email ? (
-                <a href={`mailto:${email}`}>{email}</a>
-              ) : (
-                <noscript>Turn on JavaScript to see the email address</noscript>
-              )}
-            </div>
+            <div>williamwei9857@gmail.com</div>
             <div className="flex mt-12 gap-5">
               <a href="privacy-policy" className="font-bold uppercase underline underline-offset-4 text-primary">
                 Privacy Policy
