2b949e73a916f91e7808fbb71ff4979beb308dfc
William <billywei0925@gmail.com>
2026-04-08 02:44:43 -0400

    added copy manifests to build.mjs

--- server/build.mjs
+++ server/build.mjs
@@ -1,4 +1,17 @@
 import esbuild from "esbuild";
+import fs from "fs";
+import path from "path";
+
+const copyManifests = {
+  name: "copy-manifests",
+  setup(build) {
+    build.onEnd(() => {
+      const src = path.join("src/assets/manifests");
+      const dest = path.join("dist/assets/manifests");
+      fs.cpSync(src, dest, { recursive: true });
+    });
+  },
+};
 
 esbuild.build({
   entryPoints: ["src/server.ts"],
@@ -9,5 +22,6 @@ esbuild.build({
   outfile: "dist/server.js",
   sourcemap: true,
   packages: "external",
+  plugins: [copyManifests],
 }).catch(() => process.exit(1));
 
